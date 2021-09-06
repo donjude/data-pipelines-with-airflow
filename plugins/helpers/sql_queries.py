8adc977b9e97a4b6f57c6776cf859a3f2b6fdb10
donjude <donjude83@live.com>
2021-09-06 23:07:22 +0000

    dags, sql and data quality plugin updated

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -40,3 +40,25 @@ class SqlQueries:
                extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
         FROM songplays
     """)
+    
+    check_nulls_songplays = ("""
+        SELECT COUNT(*) FROM songplays WHERE playid IS NULL    
+    """)
+    
+    check_nulls_songs = ("""
+        SELECT COUNT(*) FROM songs WHERE songid IS NULL    
+    """)
+    
+    check_nulls_artists = ("""
+        SELECT COUNT(*) FROM artists WHERE artistid IS NULL    
+    """)
+    
+    check_nulls_users = ("""
+        SELECT COUNT(*) FROM users WHERE userid IS NULL    
+    """)
+    
+    null_checks = [check_nulls_songplays, check_nulls_songs, check_nulls_artists, check_nulls_users]
+    exp_results = [0, 0, 0, 0]
+    
+    
+    
