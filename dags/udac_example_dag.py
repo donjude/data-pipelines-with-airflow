0c4e6d749be67be3e2a9642d34ac038eec0b590a
donjude <donjude83@live.com>
2021-09-28 13:52:05 +0000

    updated airflow dag

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -10,35 +10,26 @@ from helpers import SqlQueries
 # AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
-    'owner': 'udacity',
+    'owner': 'sparkify',
     'start_date': datetime(2019, 1, 12),
-<<<<<<< HEAD
-=======
     'depends_on_past': False,
     'retries': 3,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=5),
     'email_on_retry': False,
-    'catchup': False    
->>>>>>> origin/main
+    'catchup': False
 }
 
-dag = DAG('udac_example_dag',
+dag = DAG('sparkify_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
-<<<<<<< HEAD
-=======
+          schedule_interval="@hourly",
           max_active_runs=1
->>>>>>> origin/main
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-<<<<<<< HEAD
-    dag=dag
-=======
     dag=dag,
     aws_credentials_id="aws_credentials",
     redshift_conn_id="redshift",
@@ -47,14 +38,10 @@ stage_events_to_redshift = StageToRedshiftOperator(
     s3_key="log_data",
     region="us-west-2",
     extra_params="FORMAT AS JSON 's3://udacity-dend/log_json_path.json'"
->>>>>>> origin/main
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-<<<<<<< HEAD
-    dag=dag
-=======
     dag=dag,
     aws_credentials_id="aws_credentials",
     redshift_conn_id="redshift",
@@ -63,85 +50,58 @@ stage_songs_to_redshift = StageToRedshiftOperator(
     s3_key="song_data",
     region="us-west-2",
     extra_params="JSON 'auto' COMPUPDATE OFF"
->>>>>>> origin/main
 )
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
-<<<<<<< HEAD
-    dag=dag
-=======
     dag=dag,
     redshift_conn_id="redshift",
     table="songplays",
-    sql=SqlQueries.songplay_table_insert
->>>>>>> origin/main
+    sqlquery=SqlQueries.songplay_table_insert
 )
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-<<<<<<< HEAD
-    dag=dag
-=======
     dag=dag,
     redshift_conn_id="redshift",
     table="users",
-    sql=SqlQueries.user_table_insert,
+    sqlquery=SqlQueries.user_table_insert,
     truncate_table=True
->>>>>>> origin/main
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-<<<<<<< HEAD
-    dag=dag
-=======
     dag=dag,
     redshift_conn_id="redshift",
     table="songs",
-    sql=SqlQueries.song_table_insert,
+    sqlquery=SqlQueries.song_table_insert,
     truncate_table=True
->>>>>>> origin/main
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-<<<<<<< HEAD
-    dag=dag
-=======
     dag=dag,
     redshift_conn_id="redshift",
     table="artists",
-    table=SqlQueries.artist_table_insert,
+    sqlquery=SqlQueries.artist_table_insert,
     truncate_table=True
->>>>>>> origin/main
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-<<<<<<< HEAD
-    dag=dag
-=======
     dag=dag,
     redshift_conn_id="redshift",
-    table="time,
-    sql=SqlQueries.time_table_insert,
+    table="time",
+    sqlquery=SqlQueries.time_table_insert,
     truncate_table=True
->>>>>>> origin/main
 )
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
-<<<<<<< HEAD
-    dag=dag
-)
-
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
-=======
     dag=dag,
-    redshift_conn_id="redshift",
-    sql_null_checks=SqlQueries.null_checks,
-    expected_results=SqlQueries.exp_results
+    redshift_conn_id="redshift"
+#     sql_null_checks=SqlQueries.null_checks,
+#     expected_results=SqlQueries.exp_results
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
@@ -164,5 +124,3 @@ load_artist_dimension_table >> run_quality_checks
 load_time_dimension_table >> run_quality_checks
 
 run_quality_checks >> end_operator
-
->>>>>>> origin/main
