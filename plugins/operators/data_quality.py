51ad5f86cdf757779f5c21b3b4eba62ec1047d08
donjude <donjude83@live.com>
2021-09-28 14:32:59 +0000

    reset to the original data quality script

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,28 +8,15 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 redshift_conn_id="",
-                 sql_null_checks=[],
-                 expected_results=[],
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # conn_id = your-connection-name
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        
-        self.redshift_conn_id = redshift_conn_id
-        self.sql_null_checks = sql_null_checks
-        self.expected_results = expected_results
-
+        # Map params here
+        # Example:
+        # self.conn_id = conn_id
 
     def execute(self, context):
-        redshift = PostgresHook(post_conn_id=self.redshift_conn_id)
-        
-        if len(self.sql_null_checks) != len(self.expected_results):
-            raise ValueError('lengths for test and expected results do not match')
-
-
-        for i in range(len(self.sql_null_checks)):
-            result = redshift.get_first(self.sql_null_checks[i])
-            if result[0] != self.expected_results[i]:
-                raise ValueError('data quality test {} failed'.format(i))
-            else:
-                self.log.info("data quality test {} passed".format(i))
+        self.log.info('DataQualityOperator not implemented yet')
